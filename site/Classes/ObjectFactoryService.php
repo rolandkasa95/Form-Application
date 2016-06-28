553a602bc5d7d85e14197a132f2ba719fd99e1fd
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-28 14:23:03 +0300

    New: init function added, aoutoloads the paths given by the config file

---
+++ site/Classes/ObjectFactoryService.php
@@ -0,0 +1,32 @@
+<?php
+
+abstract class ObjectFactoryService{
+    public static $pdo;
+    public static $session;
+    public static $config;
+    public static $model = [];
+
+
+    /**
+     * PDO connection
+     *
+     * Connecting to the database using the PDO method
+     *
+     * @param array|null $connectParams
+     * @return PDO
+     */
+    public static function getDb(array $connectParams = null){
+        if(!self::$pdo) {
+            try{
+                $config = [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION];
+                self::$pdo = new PDO($connectParams['db']['dsn'],
+                    $connectParams['db']['user'],
+                    $connectParams['db']['pass'],
+                    $config);
+            }catch(PDOException $e){
+                echo "Error while connecting: " . $e->getMessage();
+            }
+        }
+        return self::$pdo;
+    }
+}
