b20300569ddd4fd1d002e83446b2f9589fcf7d97
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-28 16:47:52 +0300

    New: added UserModel - getUsers

--- site/Classes/Models/UserModel.php
+++ site/Classes/Models/UserModel.php
@@ -5,8 +5,21 @@ class UserModel implements ModelInteface
 
     protected $db;
 
+    /**
+     * UserModel constructor.
+     * @param $pdo
+     */
     public function __construct($pdo)
     {
         $this->db = $pdo;
+    }
+
+    public function getUsers(){
+        $sql= 'Select * from `users`';
+        try{
+            return $this->db->query($sql,PDO::FETCH_ASSOC);
+        }catch (PDOException $e){
+            echo "Errore constucting the query: " . $e->getMessage();
+        }
     }
 }
