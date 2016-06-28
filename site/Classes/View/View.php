cd784857ea2bdfab15868dd582f4cf0f40b68603
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-28 15:05:09 +0300

    New: View.php added function setResults

---
+++ site/Classes/View/View.php
@@ -0,0 +1,17 @@
+<?php
+
+class View
+{
+    public $results;
+
+
+    /**
+     * setter
+     *
+     * @param $results
+     */
+    public function setResults($results){
+        $this->results = $results;
+    }
+
+}
