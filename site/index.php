06bbf0ba13203eef9093fa0e117fbf92a36492bb
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-28 14:31:10 +0300

    New: added the Loader to the index file

--- site/index.php
+++ site/index.php
@@ -1,1 +1,10 @@
 <?php
+
+define('CLASSES', __DIR__ . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR);
+define('LAYOUTS', __DIR__ . DIRECTORY_SEPARATOR . 'Layouts' . DIRECTORY_SEPARATOR);
+
+require CLASSES . 'ObjectFactoryService.php';
+require 'Loader.php';
+Loader::init();
+
+echo 'success';
