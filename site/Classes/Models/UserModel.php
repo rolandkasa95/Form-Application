4ec78b9cfedad061ea9e378c867ce12de62a8c8f
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-28 17:00:48 +0300

    New: added UserModel - saveUser

--- site/Classes/Models/UserModel.php
+++ site/Classes/Models/UserModel.php
@@ -44,4 +44,26 @@ class UserModel implements ModelInteface
             echo "Error atenticating the user: " . $e->getMessage();
         }
     }
+
+    /**
+     * @param $data
+     */
+    public function saveUser($data){
+        $hash = password_hash($data['password'],PASSWORD_DEFAULT);
+        $sql = "INSERT INTO `users` (`username`,`passowrd`,`first_name`,`last_name`,`email`,`email_preferred_contact`,`country`) VALUES (:username, :password, :first_name, :last_name, :email, :email_preferred_contact, :country)";
+
+        try{
+            $statemet = $this->db->prepare($sql);
+            $statemet->bindParam(':username',$data['username'],PDO::PARAM_STR,50);
+            $statemet->bindParam(':password',$hash,PDO::PARAM_STR,255);
+            $statemet->bindParam(':last_name',$data['last_name'],PDO::PARAM_STR,50);
+            $statemet->bindParam(':first_name',$data['first_name'],PDO::PARAM_STR,50);
+            $statemet->bindParam(':email',$data['email'],PDO::PARAM_STR,50);
+            $statemet->bindParam(':email_preferred_contact',$data['email_preferred_contact'],PDO::PARAM_STR,1);
+            $statemet->bindParam(':country',$data['country'],PDO::PARAM_STR,100);
+            $statemet->execute();
+        }catch(PDOException $e){
+            echo 'Error ocurring registering: ' . $e->getMessage();
+        }
+    }
 }
