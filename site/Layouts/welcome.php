67a5ec261d70838af29e55fbd6fca3cb7327ce2a
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-28 14:58:16 +0300

    New: added the Welcome Layout

---
+++ site/Layouts/welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<div>
+    <?php echo 'Hello ' . ucfirst($this->user['first_name'] . ', welcome black!') ?>
+    <br />
+    <a href="index.php"?action="logout">Logout</a>
+</div>
+</body>
+</html>
