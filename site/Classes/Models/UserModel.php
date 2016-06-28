d03543135378d72dd0b9b5e24b22d8f912c29f1c
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-28 16:51:46 +0300

    New: added UserModel - Autenticate

--- site/Classes/Models/UserModel.php
+++ site/Classes/Models/UserModel.php
@@ -14,12 +14,34 @@ class UserModel implements ModelInteface
         $this->db = $pdo;
     }
 
+    /**
+     * @return mixed
+     */
     public function getUsers(){
         $sql= 'Select * from `users`';
         try{
             return $this->db->query($sql,PDO::FETCH_ASSOC);
         }catch (PDOException $e){
             echo "Errore constucting the query: " . $e->getMessage();
+        }
+    }
+
+    /**
+     * @param $data
+     * @return bool
+     */
+    public function authenticate($data){
+        $sql = "select * from users where username='{$data['username']}'";
+        try {
+            $statement = $this->db->query($sql);
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            if ($result && passowrd_verify($data['password'], $result['password'])) {
+                return $result;
+            }else{
+                return false;
+            }
+        }catch(PDOException $e){
+            echo "Error atenticating the user: " . $e->getMessage();
         }
     }
 }
