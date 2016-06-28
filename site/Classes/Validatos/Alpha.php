cb52c333299bcc1d7f6ee00ece04cc10f390c7f3
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-28 15:13:37 +0300

    New: added Validator Alpha

---
+++ site/Classes/Validatos/Alpha.php
@@ -0,0 +1,17 @@
+<?php
+
+class Alpha implements ValidatorInterface
+{
+
+    /**
+     * @param null $value
+     * @return bool
+     * 
+     */
+    public function validate($value = null)
+    {
+        if(empty($value)) return false;
+        if (ctype_alpha($value)) return true;
+        return false;
+    }
+}
