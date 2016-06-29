8ab9bd8ba7dff68189cb813897e88868ab50ee5f
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-29 14:25:17 +0300

    New: added FormBase: getStartTag

--- site/Classes/Forms/FormBase.php
+++ site/Classes/Forms/FormBase.php
@@ -8,10 +8,30 @@ abstract class FormBase
     protected $data;
     public $isValid;
 
+
+    /**
+     * FormBase constructor.
+     * @param $models
+     * @param null $params
+     */
     public function __construct($models,$params = null)
     {
         $this->models = $models;
         $this->config = $params;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStartTag(){
+        $config = $this->config;
+        $form = "<form";
+        $form .= $config['id'] ? "id = \"{$config['id']}\"" :null;
+        $form .= $config['name'] ? " name=\"{$config['name']}\"" : null;
+        $form .= $config['action'] ? " action=\"{$config['action']}\"" : null;
+        $form .= $config['method'] ? " method=\"{$config['method']}\"" : null;
+        $form .= '>';
+        return $form;
     }
 
 
