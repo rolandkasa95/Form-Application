ffb37b7fd390ac2442b25d151bca5b47f48e5b0b
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-29 14:21:07 +0300

    New: added FormComon : __constuct()

--- site/Classes/Forms/FormCommon.php
+++ site/Classes/Forms/FormCommon.php
@@ -1,6 +1,38 @@
 <?php
 
-class FormCommon
+require 'FormBase.php';
+
+abstract class FormCommon extends FormBase
 {
+
+    /**
+     * FormCommon constructor.
+     * @param $models
+     * @param $param
+     *
+     * @internal $param
+     */
+    public function __construct($models,$param)
+    {
+        /**
+         * Calling the FormBase construct
+         */
+        parent::__construct($models,$param);
+        $token = md5(time());
+        $this->addField([
+            'name' => 'token',
+            'type' => 'hidden',
+            'value' => $token,
+            'priority' => 99,
+            'validator' => 'token',
+        ]);
+
+        $this->addField([
+            'name' => 'submit',
+            'type' => 'submit',
+            'value' => 'submit',
+            'priority' => 100,
+        ]);
+    }
 
 }
