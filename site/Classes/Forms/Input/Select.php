063e338326f349ab1b2d710dc4dcd09b17d823d6
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-29 16:16:53 +0300

    New: added Input/Select: __construct + getInput

--- site/Classes/Forms/Input/Select.php
+++ site/Classes/Forms/Input/Select.php
@@ -1,6 +1,30 @@
 <?php
 
-class Select
+class Select extends BaseInput implements InputInterface
 {
+    protected $multiple = false;
+    protected $options = [];
 
+    /**
+     * Select constructor.
+     */
+    public function __construct()
+    {
+        $this->valid = false;
+        $this->required = false;
+    }
+
+    public function getInput()
+    {
+        $select = "<select ";
+        $select .= $this->name ? " name=\"$this->name\"" : null;
+        $select .= $this->required ? " required" : null;
+        $select .= $this->multiple ? " multiple" : null;
+        $select .= ">";
+        foreach ($this->options as $option){
+            $select .= $option;
+        }
+        $select .= "</select>";
+        return $select;
+    }
 }
