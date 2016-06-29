6c9d2a9e3227ead3ede193195ffaa9ee81c0e3b7
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-29 15:16:09 +0300

    New: added RegisterForm: Username + Password

--- site/Classes/Forms/RegisterForm.php
+++ site/Classes/Forms/RegisterForm.php
@@ -1,6 +1,48 @@
 <?php
 
-class RegisterForm
+class RegisterForm extends FormCommon implements FormInterface
 {
+    public function __construct($models)
+    {
+        $param = [
+            'name' => 'register',
+            'id' => 'form1',
+            'method' => 'post',
+            'action' => 'index.php'
+        ];
+        parent::__construct($models, $param);
 
+        $this->addField([
+            'label' => 'Username',
+            'type' => 'text',
+            'name' => 'username',
+            'priority' => 1,
+            'required' => true,
+            'value' => '',
+            'validator' => [
+                'StringLength' => [
+                    'minimum' => 5,
+                    'maximum' => 40
+                ],
+                'alnum',
+                'required',
+            ],
+        ]);
+
+        $this->addField([
+            'label' => 'Password',
+            'type' => 'password',
+            'name' => 'password',
+            'priority' => 2,
+            'required' => true,
+            'value' => '',
+            'validator' => [
+                'StringLength' => [
+                    'minimum' => 8,
+                    'maximum' => 100,
+                ],
+                'required',
+            ],
+        ]);
+    }
 }
