5d99abf8e1c63c73f46bb7c7573fbb2497572bbb
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-29 15:46:32 +0300

    New: added Input/Hidden: __construct() + getInput

--- site/Classes/Forms/Input/Hidden.php
+++ site/Classes/Forms/Input/Hidden.php
@@ -2,5 +2,12 @@
 
 class Hidden extends BaseInput implements InputInterface
 {
-    
+    public function __construct(){
+        $this->type = 'hidden';
+    }
+
+    public function getInput()
+    {
+        return "<input type=\"" . $this->type . "\" name=\"" . $this->name . "\" value=\"" . $this->value . "\"";
+    }
 }
