192fd996093326c963106c29491f3d07fc7c4269
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-29 15:28:04 +0300

    New: added Input/BaseInput: setValue

---
+++ site/Classes/Forms/Input/BaseInput.php
@@ -0,0 +1,22 @@
+<?php
+
+abstract class BaseInput
+{
+    protected $type;
+    protected $name;
+    protected $label;
+    protected $value;
+    protected $required;
+    protected $valid;
+    protected $validators;
+
+
+    /**
+     * @param $param
+     * @return $this
+     */
+    public function setValue($param){
+        $this->value = $param;
+        return $this;
+    }
+}
