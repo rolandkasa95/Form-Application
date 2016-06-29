361821a76feb9c184f4d4ac8b74719c6c462e616
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-29 16:04:37 +0300

    New: added Input/Options: getOption + getOptions

--- site/Classes/Forms/Input/Option.php
+++ site/Classes/Forms/Input/Option.php
@@ -2,5 +2,35 @@
 
 class Option
 {
+    protected $label;
+    protected $disabled = false;
+    protected $selected = false;
+    protected $value;
+    protected $optionString;
+
+    /**
+     * @return string
+     */
+    public function getOption(){
+        $option = "<option ";
+        $option .= $this->value ? " value=\"{$this->value}\"" :null;
+        $option .= $this->disabled ? " disabled" :null;
+        $option .= $this->label ? "label=\"{$this->label}\"" : null;
+        $option .= $this->selected ? " selected" : null;
+        $option .= ">" . $this->optionString . "</option>";
+        return $option;
+    }
+
+    public function getOptions($options){
+        $result = null;
+        foreach ($options as $option) {
+            $value = ucwords($option);
+            $this->value = $value;
+            $this->optionString = $value;
+            $result[] = $this->getOption($option);
+        }
+        return $result ?:false;
+    }
+
 
 }
