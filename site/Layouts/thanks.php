dddd5d339a045d0eb2aeec680a3a8269099e122d
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-05 16:18:29 +0300

    New: added a back reference to thanks.php

--- site/Layouts/thanks.php
+++ site/Layouts/thanks.php
@@ -2,6 +2,9 @@
 <body>
 <div id="content">
     <?php echo 'Thanks for your registration'?>
-</div>
+    <form action="../index.php" method="get">
+        <a href="javascript: window.history.go(-1)">Back</a>
+    </form>
+</div>  
 </body>
 </html>
