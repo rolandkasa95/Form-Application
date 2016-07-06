e4b4af5c863a7d006dec85e5439374db33de3a8e
rolandkasa95 <rolandkasa95@gmail.com>
2016-07-06 13:15:24 +0300

    Update: renamed Validators

--- site/Layouts/register.php
+++ site/Layouts/register.php
@@ -9,6 +9,7 @@
         <?php echo $field->getInput() . '</br>'?>
     <?php endforeach ?>
     <?php echo $this->form->getEndTag()?>
+    <a href="../index.php" style="border-style: double ;padding: 8px 10px 10px 8px; border-radius: 20px;">Back to Login</a>
 </div>
 </body>
 </html>
